aa358334fc9eaec3976d5983e757c9af8d23163b
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-20 07:54:07 +0000

    gnutls: update to 3.8.10

--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gnutls"
-PKG_VERSION="3.8.3"
-PKG_SHA256="f74fc5954b27d4ec6dfbb11dea987888b5b124289a3703afcada0ee520f4173e"
+PKG_VERSION="3.8.10"
+PKG_SHA256="db7fab7cce791e7727ebbef2334301c821d79a550ec55c9ef096b610b03eb6b7"
 PKG_LICENSE="LGPL2.1"
 PKG_SITE="https://gnutls.org"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/gnutls/v${PKG_VERSION:0:3}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
@@ -14,7 +14,6 @@ PKG_LONGDESC="A library which provides a secure layer over a reliable transport 
 
 PKG_CONFIGURE_OPTS_COMMON="--disable-doc \
                            --disable-full-test-suite \
-                           --disable-guile \
                            --disable-libdane \
                            --disable-padlock \
                            --disable-rpath \
