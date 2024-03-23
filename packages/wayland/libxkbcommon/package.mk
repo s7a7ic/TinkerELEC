e5c2dfc96228244f1fb0ef12b5c2513d635f9d9d
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-23 23:06:42 +0000

    libxkbcommon: update to 1.10.0 and PKG_URL
    news:
    - https://github.com/xkbcommon/libxkbcommon/blob/master/NEWS.md

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxkbcommon"
-PKG_VERSION="1.6.0"
-PKG_SHA256="0edc14eccdd391514458bc5f5a4b99863ed2d651e4dd761a90abf4f46ef99c2b"
+PKG_VERSION="1.10.0"
+PKG_SHA256="0427585a4d6ca17c9fc1ac4b539bf303348e9080af70c5ea402503bc370a9631"
 PKG_LICENSE="MIT"
 PKG_SITE="https://xkbcommon.org"
-PKG_URL="https://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain xkeyboard-config libxml2"
 PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
 
