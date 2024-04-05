92557c0e5f244f3ee1ff939e255bd50a64294bce
Rudi Heitbaum <rudi@heitbaum.com>
2024-04-05 08:18:11 +0000

    gmmlib: update to 22.8.1

--- packages/multimedia/gmmlib/package.mk
+++ packages/multimedia/gmmlib/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmmlib"
-PKG_VERSION="22.3.18"
-PKG_SHA256="381f7ad104e3d0d8dd6c3ec0c8304e410a479c439e72db35c83fb7e2b0ec556f"
+PKG_VERSION="22.8.1"
+PKG_SHA256="9b8eac1891650021ded26b72585e7a2c702a3ba47565c968feabd14ab38d18f7"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
