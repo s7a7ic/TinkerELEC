7ac532282c492fbe3dc124ef3381588c776b8bac
Rudi Heitbaum <rudi@heitbaum.com>
2024-06-14 12:17:55 +0000

    rust: update to 1.81.0

--- packages/rust/rust/package.mk
+++ packages/rust/rust/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rust"
-PKG_VERSION="1.78.0"
-PKG_SHA256="ff544823a5cb27f2738128577f1e7e00ee8f4c83f2a348781ae4fc355e91d5a9"
+PKG_VERSION="1.81.0"
+PKG_SHA256="872448febdff32e50c3c90a7e15f9bb2db131d13c588fe9071b0ed88837ccfa7"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-src.tar.gz"
