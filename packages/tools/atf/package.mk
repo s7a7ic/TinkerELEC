cfd37e0afa615b3199fabed09e91d5b2c67b90ad
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-05 10:19:15 +0000

    atf: update to 2.13.0
    as of atf 2.12.0 - sun50i_a64 builds use LTO, include -ffat-lto-objects to support this
    
    alternatively (not done) is to build sun50i_a64 with ENABLE_LTO=0 SUNXI_AMEND_DTB=0

--- packages/tools/atf/package.mk
+++ packages/tools/atf/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC
 
 PKG_NAME="atf"
-PKG_VERSION="2.10.0"
-PKG_SHA256="696b8e53923aac4474532da7dd681f0bd044b329732facd65aeabea3e61adca9"
+PKG_VERSION="2.13.0"
+PKG_SHA256="28bc15daeeed000ecd30819ecc4851bf9ffc2d33e1d4553a71985c17f47a999e"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="https://github.com/ARM-software/arm-trusted-firmware"
-PKG_URL="https://github.com/ARM-software/arm-trusted-firmware/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/TrustedFirmware-A/trusted-firmware-a/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="ARM Trusted Firmware is a reference implementation of secure world software, including a Secure Monitor executing at Exception Level 3 and various Arm interface standards."
 PKG_TOOLCHAIN="manual"
@@ -20,10 +20,13 @@ if [ "${ATF_PLATFORM}" = "rk3399" ]; then
 fi
 
 make_target() {
+  # As of atf 2.11.0 - the supported compile for .S is gcc (not as.)
+  unset AS
   if [ "${ATF_PLATFORM}" = "imx8mq" ]; then
     CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" CFLAGS="" make PLAT=${ATF_PLATFORM} LOG_LEVEL=0 bl31
   else
-    CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" CFLAGS="" make PLAT=${ATF_PLATFORM} bl31
+    # as of atf 2.12.0 - sun50i_a64 builds use LTO, include -ffat-lto-objects to support this
+    CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" CFLAGS="-ffat-lto-objects" make PLAT=${ATF_PLATFORM} bl31
   fi
 }
 
