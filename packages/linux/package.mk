9f2e74239179ae237088d1f8eb0a5d20d5943172
Christian Hewitt <christianshewitt@gmail.com>
2025-06-08 09:42:03 +0000

    linux: add Rockchip kernel and patches for Linux 6.16.y and 6.17.y
    Patches for RK356X, RK3576 and RK3588 are placed in a 'rockchip'
    folder under the Linux package dir. Boards use a common aarch64
    arch defconfig.
    
    Patches for RK3288, RK3328 and RK3399 are moved to a 'rockchip-old'
    folder under the Linux package dir. Boards continue to use device
    level 6.16.y defconfigs to ensure all drivers are present, except
    for RK3288 which has been moved to the project level folder to be
    the common arm arch defconfig.
    
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,11 +29,23 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="raspberrypi rtlwifi/6.13 rtlwifi/6.14 rtlwifi/6.15"
     ;;
+  rockchip)
+    PKG_VERSION="76eeb9b8de9880ca38696b2fb56ac45ac0a25c6c" # 6.17-rc5
+    PKG_SHA256="b2ff7ef05755dadd0cfc526ef59ec80954a89ec6dc71e978ea09cb0420551950"
+    PKG_URL="https://github.com/chewitt/linux/archive/${PKG_VERSION}.tar.gz"
+    PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
+    PKG_PATCH_DIRS="default rockchip rtlwifi/6.17"
+    ;;
   *)
     PKG_VERSION="6.12.23"
     PKG_SHA256="d8d95404f8deeb7ff6992c0df855025062e9e8182bca6daa27ef2e9275d27749"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
+    case ${DEVICE} in
+      RK3288|RK3328|RK3399)
+        PKG_PATCH_DIRS+=" rockchip-old"
+        ;;
+    esac
     ;;
 esac
 
