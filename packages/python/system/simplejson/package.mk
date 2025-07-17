11407ae9f0c591fa48c925c32738aad59329844f
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-07-17 10:58:44 +0200

    simplejson: update to 3.20.1

--- packages/python/system/simplejson/package.mk
+++ packages/python/system/simplejson/package.mk
@@ -3,27 +3,14 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="simplejson"
-PKG_VERSION="3.19.2"
-PKG_SHA256="9eb442a2442ce417801c912df68e1f6ccfcd41577ae7274953ab3ad24ef7d82c"
+PKG_VERSION="3.20.1"
+PKG_SHA256="e64139b4ec4f1f24c142ff7dcafe55a22b811a74d86d66560c8815687143037d"
 PKG_LICENSE="OSS"
 PKG_SITE="http://pypi.org/project/simplejson"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3 distutilscross:host"
+PKG_DEPENDS_TARGET="toolchain Python3 setuptools:host"
 PKG_LONGDESC="A simple, fast, complete, correct and extensible JSON encoder and decoder for Python 2.5+."
-PKG_TOOLCHAIN="manual"
-
-pre_make_target() {
-  export PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr"
-  export LDSHARED="${CC} -shared"
-}
-
-make_target() {
-  python3 setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python3 setup.py install --root=${INSTALL} --prefix=/usr
-}
+PKG_TOOLCHAIN="python"
 
 post_makeinstall_target() {
   python_remove_source
