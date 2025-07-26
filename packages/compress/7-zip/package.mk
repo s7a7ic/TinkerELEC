1d89a1c1305e2d9d7f1894e635c8a5fbded0c1e7
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-26 14:38:03 +0000

    7-zip: update to 25.00

--- packages/compress/7-zip/package.mk
+++ packages/compress/7-zip/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="7-zip"
-PKG_VERSION="23.01"
-PKG_SHA256="356071007360e5a1824d9904993e8b2480b51b570e8c9faf7c0f58ebe4bf9f74"
+PKG_VERSION="25.00"
+PKG_SHA256="bff9e69b6ca73a5b8715d7623870a39dc90ad6ce1f4d1070685843987af1af9b"
 PKG_LICENSE="7-Zip"
 PKG_SITE="https://www.7-zip.org"
 PKG_URL="https://www.7-zip.org/a/7z${PKG_VERSION/./}-src.tar.xz"
