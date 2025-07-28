4e75ecca835633440cb8d69c6143623c5e7fb2ee
Matthias Reichl <hias@horus.com>
2025-07-28 12:13:10 +0200

    espeak-ng: update to 348599e
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/audio/espeak-ng/package.mk
+++ packages/audio/espeak-ng/package.mk
@@ -2,31 +2,28 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="espeak-ng"
-PKG_VERSION="1.51.1"
-PKG_SHA256="0823df5648659dcb67915baaf99118dcc8853639f47cadaa029c174bdd768d20"
+PKG_VERSION="348599e02bf0db105ac6fd07693e4047f614a58e"
+PKG_SHA256="f12884158f1e6c3727bcc340f2459a0a5481f8e02f7c35dbd69dae936f064d1c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/espeak-ng/espeak-ng"
-PKG_URL="https://github.com/espeak-ng/espeak-ng/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/espeak-ng/espeak-ng/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="gcc:host"
 PKG_DEPENDS_TARGET="toolchain espeak-ng:host"
 PKG_LONGDESC="eSpeak NG is an open source speech synthesizer that supports more than a hundred languages and accents"
-PKG_TOOLCHAIN="autotools"
+PKG_BUILD_FLAGS="+pic"
 
-make_host() {
-  mkdir phsource dictsource
-  (cd dictsource; ln -s ../../dictsource/* .)
-  (cd phsource; ln -s ../../phsource/* .)
-  cp -aP ../espeak-ng-data .
-  make DESTDIR=`pwd` -j1
-}
+PKG_CMAKE_OPTS_HOST="-DBUILD_SHARED_LIBS=OFF \
+                     -DCOMPILE_INTONATIONS=OFF \
+                     -DENABLE_TESTS=OFF"
 
-make_target() {
-  make src/espeak-ng src/speak-ng
-}
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=ON \
+                       -DCOMPILE_INTONATIONS=ON \
+                       -DENABLE_TESTS=OFF \
+                       -DNativeBuild_DIR=${TOOLCHAIN}/bin"
 
-makeinstall_target() {
-  make src/espeak-ng src/speak-ng
-  make install-exec DESTDIR=${INSTALL}
-  mkdir -p ${INSTALL}/usr/share/espeak-ng-data
-  cp -prf ${TOOLCHAIN}/share/espeak-ng-data ${INSTALL}/usr/share
+post_makeinstall_target() {
+  safe_remove ${INSTALL}/usr/share/vim
+
+  # add symlink for backwards compatibility with old programs
+  ln -sf espeak-ng ${INSTALL}/usr/bin/espeak
 }
