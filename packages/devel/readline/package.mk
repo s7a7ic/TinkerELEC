01d8eaba4d76a25db15216177eeed8339bb6c0c4
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-07-28 13:24:18 +0200

    readline: update to 8.2.13

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="readline"
-PKG_VERSION="8.2"
-PKG_SHA256="3feb7171f16a84ee82ca18a36d7b9be109a52c04f492a053331d7d1095007c35"
+PKG_VERSION="8.2.13"
+PKG_SHA256="0e5be4d2937e8bd9b7cd60d46721ce79f88a33415dd68c2d738fb5924638f656"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.gnu.org/software/readline/"
-PKG_URL="http://ftpmirror.gnu.org/readline/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain ncurses"
+PKG_URL="https://ftpmirror.gnu.org/readline/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="autotools:host gcc:host ncurses"
 PKG_LONGDESC="The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in."
 PKG_BUILD_FLAGS="+pic"
 
