65364073a3d0077dd3746e3f4e685d6b0442a599
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-07-28 15:40:36 +0200

    mariadb-connector-c: update to 3.4.6

--- packages/databases/mariadb-connector-c/package.mk
+++ packages/databases/mariadb-connector-c/package.mk
@@ -2,20 +2,22 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mariadb-connector-c"
-PKG_VERSION="3.3.9"
-PKG_SHA256="062b9ec5c26cbb236a78f0ba26981272053f59bdfc113040bab904a9da36d31f"
+PKG_VERSION="3.4.6"
+PKG_SHA256="27b57790896b7464e1b87fb29bad49e31ee36fdb6942e5c86284c6af9630be0e"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://mariadb.org/"
 PKG_URL="https://github.com/mariadb-corporation/mariadb-connector-c/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
-PKG_LONGDESC="mariadb-connector: library to conntect to mariadb/mysql database server"
+PKG_LONGDESC="mariadb-connector: library to connect to mariadb/mysql database server"
 PKG_BUILD_FLAGS="-gold"
 
 PKG_CMAKE_OPTS_TARGET="-DWITH_EXTERNAL_ZLIB=ON
+                       -DWITH_UNIT_TESTS=OFF
                        -DCLIENT_PLUGIN_DIALOG=STATIC
                        -DCLIENT_PLUGIN_MYSQL_CLEAR_PASSWORD=STATIC
                        -DCLIENT_PLUGIN_MYSQL_OLD_PASSWORD=STATIC
                        -DCLIENT_PLUGIN_REMOTE_IO=OFF
+                       -DDEFAULT_SSL_VERIFY_SERVER_CERT=OFF
                       "
 
 post_makeinstall_target() {
