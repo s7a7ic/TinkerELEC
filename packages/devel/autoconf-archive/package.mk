5f17d1861ac279af138466b1485b57942760904f
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-07-29 00:18:59 +0200

    autoconf-archive: update to 2024.10.16

--- packages/devel/autoconf-archive/package.mk
+++ packages/devel/autoconf-archive/package.mk
@@ -3,20 +3,21 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="autoconf-archive"
-PKG_VERSION="2023.02.20"
-PKG_SHA256="71d4048479ae28f1f5794619c3d72df9c01df49b1c628ef85fde37596dc31a33"
+PKG_VERSION="2024.10.16"
+PKG_SHA256="7bcd5d001916f3a50ed7436f4f700e3d2b1bade3ed803219c592d62502a57363"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/autoconf-archive/"
-PKG_URL="http://ftpmirror.gnu.org/autoconf-archive/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://ftpmirror.gnu.org/autoconf-archive/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="autoconf-archive is an package of m4 macros"
+PKG_BUILD_FLAGS="-cfg-libs:host"
 
 PKG_CONFIGURE_OPTS_HOST="--target=${TARGET_NAME} --prefix=${TOOLCHAIN}"
 
 makeinstall_host() {
-# make install
+  # make install
   make prefix=${SYSROOT_PREFIX}/usr install
 
-# remove problematic m4 file
+  # remove problematic m4 file
   rm -rf ${SYSROOT_PREFIX}/usr/share/aclocal/ax_prog_cc_for_build.m4
 }
