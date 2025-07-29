04590ef169ccc56e5c907405de37fad4dee0d9c0
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-07-29 17:36:44 +0200

    xz: revert to 5.4.6

--- packages/compress/xz/package.mk
+++ packages/compress/xz/package.mk
@@ -3,23 +3,30 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xz"
-PKG_VERSION="5.8.1"
-PKG_SHA256="0b54f79df85912504de0b14aec7971e3f964491af1812d83447005807513cd9e"
+PKG_VERSION="5.4.6"
+PKG_SHA256="b92d4e3a438affcf13362a1305cd9d94ed47ddda22e456a42791e630a5644f5c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://tukaani.org/xz/"
 PKG_URL="https://github.com/tukaani-project/xz/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="autotools:host gcc:host"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A free general-purpose data compression software with high compression ratio."
-PKG_BUILD_FLAGS="+pic -sysroot"
+PKG_BUILD_FLAGS="+pic +pic:host"
 PKG_TOOLCHAIN="configure"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-                           --disable-shared \
-                           --disable-doc \
-                           --disable-lzmadec \
-                           --disable-lzmainfo \
-                           --disable-lzma-links \
-                           --disable-scripts \
-                           --disable-xz \
-                           --disable-xzdec \
+# never build shared or k0p happens when building
+# on fedora due to host selinux/liblzma
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static \
+                         --disable-lzmadec \
+                         --disable-lzmainfo \
+                         --enable-lzma-links \
+                         --disable-nls \
+                         --disable-scripts \
+                         --enable-symbol-versions=no"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static \
                            --enable-symbol-versions=no"
+
+post_makeinstall_target() {
+  rm -rf ${INSTALL}
+}
