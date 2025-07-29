LibreELEC/LibreELEC.tv

nspr: replace toolchain depend
Signed-off-by: Ian Leonard <antonlacon@gmail.com>

packages/security/nspr/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="4.35"
 PKG_LICENSE="Mozilla Public License"
 PKG_SITE="http://www.linuxfromscratch.org/blfs/view/svn/general/nspr.html"
 PKG_DEPENDS_HOST="ccache:host"
-PKG_DEPENDS_TARGET="toolchain nss:host nspr:host"
+PKG_DEPENDS_TARGET="autoconf:host gcc:host nss:host nspr:host"
 PKG_DEPENDS_UNPACK="nss"
 PKG_LONGDESC="Netscape Portable Runtime (NSPR) provides a platform-neutral API for system level and libc like functions"
 PKG_TOOLCHAIN="configure"

s7a7ic/TinkerELEC

packages/security/nspr/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="4.37"
 PKG_LICENSE="Mozilla Public License"
 PKG_SITE="http://www.linuxfromscratch.org/blfs/view/svn/general/nspr.html"
 PKG_DEPENDS_HOST="ccache:host"
-PKG_DEPENDS_TARGET="toolchain nss:host nspr:host"
+PKG_DEPENDS_TARGET="autotools:host gcc:host nss:host nspr:host"
 PKG_DEPENDS_UNPACK="nss"
 PKG_LONGDESC="Netscape Portable Runtime (NSPR) provides a platform-neutral API for system level and libc like functions"
 PKG_TOOLCHAIN="configure"