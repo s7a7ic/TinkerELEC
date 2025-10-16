524c185ef8584e86baf3420eb88d042cae951e54
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-10-16 17:40:58 +0200

    mesa: update to 25.2.5
    The following options are now deprecated:
    
    - DEPRECATION: Option 'gallium-xa' is deprecated and dropped
      - is set to true for vmware otherwise was false and defaults to false

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="25.1.9"
-PKG_SHA256="412df33a1bb3c785ed698555a3972118a37c458e7accf6ae53f4bb87b3db454a"
+PKG_VERSION="25.2.5"
+PKG_SHA256="bb6243e7a6f525febfa1e6ab50827ca4d4bfdad73812377b0ca9b6c50998b03e"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
@@ -96,12 +96,6 @@ if [ "${VAAPI_SUPPORT}" = "yes" ] && listcontains "${GRAPHIC_DRIVERS}" "(r600|ra
                            -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc,av1dec,av1enc,vp9dec"
 else
   PKG_MESON_OPTS_TARGET+=" -Dgallium-va=disabled"
-fi
-
-if listcontains "${GRAPHIC_DRIVERS}" "vmware"; then
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=enabled"
-else
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=disabled"
 fi
 
 if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
