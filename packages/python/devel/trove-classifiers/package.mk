34f3941487268f019ccd9f787516be63fea342fb
Rudi Heitbaum <rudi@heitbaum.com>
2025-10-16 13:45:43 +0000

    trove-classifiers: initial package

---
+++ packages/python/devel/trove-classifiers/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="trove-classifiers"
+PKG_VERSION="2025.9.11.17"
+PKG_SHA256="1937c1004bdc176a140f0a6fa0a372e7dc1d93b1f5ba234cdf80d9371ac3818c"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://github.com/pypa/trove-classifiers"
+PKG_URL="https://github.com/pypa/trove-classifiers/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="Python3:host setuptools:host"
+PKG_LONGDESC="Canonical source for classifiers on PyPI"
+PKG_TOOLCHAIN="python"
