dde4b2c7ae2c20be91010043f988f3d4720d30d9
Matthias Reichl <hias@horus.com>
2025-11-14 22:57:34 +0100

    linux (RPi): update to 6.12.58-954129f
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,8 +23,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="default"
     ;;
   raspberrypi)
-    PKG_VERSION="78fd5c93f1568d0dac7c56ee3d497739a63b058e" # 6.12.56
-    PKG_SHA256="b4c9cd51586c21f32f5a6750c35e999270a5bbe6da7ab3ceaef0cbc3a1f91ab3"
+    PKG_VERSION="954129f16c200e41a00ebebe2e22efc01b243538" # 6.12.58
+    PKG_SHA256="e1b39f094f1f68b9070711b39928ebd996bf8139b7238767bbba2948aba90003"
     PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="raspberrypi rtlwifi/6.13 rtlwifi/6.14 rtlwifi/6.15 rtlwifi/6.18"
