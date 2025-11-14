LibreELEC/LibreELEC.tv

Rockchip: bootloader: cosmetic update

projects/Rockchip/bootloader/update.sh
@@ -39,7 +39,7 @@ fi
     esac
 
     if [ "${dtb}" != "${new_dtb}" -a -f ${SYSTEM_ROOT}/usr/share/bootloader/${new_dtb} ]; then
-      echo -n "Replacing ${dtb} with ${new_dtb} ... "
+      echo -n "Replacing ${dtb} with ${new_dtb}... "
       cp -p ${SYSTEM_ROOT}/usr/share/bootloader/${new_dtb} ${BOOT_ROOT} && \
       sed -e "s/FDT \/${dtb}/FDT \/${new_dtb}/g" \
           -i ${BOOT_ROOT}/extlinux/extlinux.conf && \
@@ -51,15 +51,15 @@ fi
         cp -p ${SYSTEM_ROOT}/usr/share/bootloader/${dtb} ${BOOT_ROOT}
         echo "done"
       elif [ "$(grep -c "FDT /${dtb}" ${BOOT_ROOT}/extlinux/extlinux.conf)" -ne 0 ]; then
-	 non_existend_dtb="${dtb}"
+        non_existend_dtb="${dtb}"
       fi
     fi
   done
 
 # update bootloader
  if [ -f ${SYSTEM_ROOT}/usr/share/bootloader/u-boot-rockchip.bin ]; then
-    echo -n "Updating fit image u-boot-rockchip.bin ... "
-    dd if=${SYSTEM_ROOT}/usr/share/bootloader/u-boot-rockchip.bin of=${BOOT_DISK} bs=32k seek=1 conv=fsync,notrunc &>/dev/null
+    echo -n "Updating U-Boot... "
+    dd if=${SYSTEM_ROOT}/usr/share/bootloader/u-boot-rockchip.bin of=${BOOT_DISK} bs=32k seek=1 conv=fsync &>/dev/null
     echo "done"
   fi
 
@@ -74,4 +74,3 @@ fi
     echo "Please check documentation to find out which boards are supported by this package."
     sleep 10
   fi
-

s7a7ic/TinkerELEC

projects/Rockchip/bootloader/update.sh
@@ -39,7 +39,7 @@ fi
     esac
 
     if [ "${dtb}" != "${new_dtb}" -a -f ${SYSTEM_ROOT}/usr/share/bootloader/${new_dtb} ]; then
-      echo -n "Replacing ${dtb} with ${new_dtb} ... "
+      echo -n "Replacing ${dtb} with ${new_dtb}... "
       cp -p ${SYSTEM_ROOT}/usr/share/bootloader/${new_dtb} ${BOOT_ROOT} && \
       sed -e "s/FDT \/${dtb}/FDT \/${new_dtb}/g" \
           -i ${BOOT_ROOT}/extlinux/extlinux.conf && \
@@ -51,15 +51,15 @@ fi
         cp -p ${SYSTEM_ROOT}/usr/share/bootloader/${dtb} ${BOOT_ROOT}
         echo "done"
       elif [ "$(grep -c "FDT /${dtb}" ${BOOT_ROOT}/extlinux/extlinux.conf)" -ne 0 ]; then
-	 non_existent_dtb="${dtb}"
+        non_existent_dtb="${dtb}"
       fi
     fi
   done
 
 # update bootloader
  if [ -f ${SYSTEM_ROOT}/usr/share/bootloader/u-boot-rockchip.bin ]; then
-    echo -n "Updating fit image u-boot-rockchip.bin ... "
-    dd if=${SYSTEM_ROOT}/usr/share/bootloader/u-boot-rockchip.bin of=${BOOT_DISK} bs=32k seek=1 conv=fsync,notrunc &>/dev/null
+    echo -n "Updating U-Boot... "
+    dd if=${SYSTEM_ROOT}/usr/share/bootloader/u-boot-rockchip.bin of=${BOOT_DISK} bs=32k seek=1 conv=fsync &>/dev/null
     echo "done"
   fi
 
@@ -67,11 +67,9 @@ fi
   sync
   mount -o remount,ro ${BOOT_ROOT}
 
-# warning if device tree was not updated
+# warn if device tree was not updated
   if [ -n "${non_existent_dtb}" ]; then
-    echo "The device tree ${non_existent_dtb} your installation uses does not exist in this update package."
-    echo "The updated system will continue to use the device tree from the previous system and your installation might be broken."
-    echo "Please check documentation to find out which boards are supported by this package."
+    echo "WARNING: The device-tree file ${non_existent_dtb} was not found in the update"
+    echo "package and has not been updated. Your device may not boot or work correctly!"
     sleep 10
   fi
-