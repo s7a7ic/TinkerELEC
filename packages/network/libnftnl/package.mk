a20fd2cf7360c81fea2fcf117e450f4f8720bb42
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-16 01:56:04 +0000

    libnftnl: change to shared library
    - iptables uses this library
    - libmnl referenced by this library is shared
    - nftables will reference this library
    - docker 29.0.0+ will reference this library
    
    before change:
    /usr/sbin/xtables-nft-multi  328624 bytes
    after change:
    /usr/sbin/xtables-nft-multi  214304 bytes
    /usr/lib/libnftnl.so.11.6.0  210672 bytes
    
    increase on x86_64 is 96K if taking as-is iptables in to account

--- packages/network/libnftnl/package.mk
+++ packages/network/libnftnl/package.mk
@@ -10,5 +10,3 @@ PKG_SITE="https://netfilter.org/projects/libnftnl"
 PKG_URL="https://netfilter.org/projects/libnftnl/files/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="autotools:host gcc:host libmnl"
 PKG_LONGDESC="A userspace library providing a low-level netlink programming interface (API) to the in-kernel nf_tables subsystem."
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
