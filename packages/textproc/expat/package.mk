e68fea2b6f51574b13e3c506c3c32f60e3604245
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-11-24 11:46:52 +0100

    packages/textproc: update from master
    update packages to version from current LibreELEC.tv master branch

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,13 +3,13 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="expat"
-PKG_VERSION="2.6.2"
-PKG_SHA256="ee14b4c5d8908b1bec37ad937607eab183d4d9806a08adee472c3c3121d27364"
+PKG_VERSION="2.7.3"
+PKG_SHA256="71df8f40706a7bb0a80a5367079ea75d91da4f8c65c58ec59bcdfbf7decdab9f"
 PKG_LICENSE="OSS"
 PKG_SITE="https://libexpat.github.io"
 PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_HOST="toolchain:host"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_HOST="cmake:host ninja:host"
+PKG_DEPENDS_TARGET="cmake:host ninja:host gcc:host"
 PKG_LONGDESC="Expat is an XML parser library written in C."
 
 PKG_CMAKE_OPTS_TARGET="-DEXPAT_BUILD_DOCS=OFF \
@@ -17,6 +17,7 @@ PKG_CMAKE_OPTS_TARGET="-DEXPAT_BUILD_DOCS=OFF \
                        -DEXPAT_BUILD_EXAMPLES=OFF \
                        -DEXPAT_BUILD_TESTS=OFF \
                        -DEXPAT_SHARED_LIBS=ON"
+
 PKG_CMAKE_OPTS_HOST="-DEXPAT_BUILD_DOCS=OFF \
                      -DEXPAT_BUILD_TOOLS=OFF \
                      -DEXPAT_BUILD_EXAMPLES=OFF \
