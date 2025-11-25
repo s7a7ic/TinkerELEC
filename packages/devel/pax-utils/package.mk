df6fb504c1657021045c6ba2b715144c13396097
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-25 12:31:04 +0000

    pax-utils: update to 1.3.10

--- packages/devel/pax-utils/package.mk
+++ packages/devel/pax-utils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pax-utils"
-PKG_VERSION="1.3.9"
-PKG_SHA256="468c597c103f079775dbd556cf9d7f150466c0b1b9b44b5107dff0eb5e20c885"
+PKG_VERSION="1.3.10"
+PKG_SHA256="e4813381dd3264c08d9693ef34b87248558acc5e63c55941dcfeca6dd62627c2"
 PKG_LICENSE="GPL-2.0"
 PKG_SITE="https://wiki.gentoo.org/wiki/Hardened/PaX_Utilities"
 PKG_URL="https://gitweb.gentoo.org/proj/pax-utils.git/snapshot/pax-utils-${PKG_VERSION}.tar.bz2"
