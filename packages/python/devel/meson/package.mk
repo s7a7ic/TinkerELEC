4d73b9c52c6be1c7a85fd0204d99d1f8f89d4b00
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-08 20:34:14 +0000

    meson: update to 1.10.0

--- packages/python/devel/meson/package.mk
+++ packages/python/devel/meson/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="meson"
-PKG_VERSION="1.8.2"
-PKG_SHA256="c105816d8158c76b72adcb9ff60297719096da7d07f6b1f000fd8c013cd387af"
+PKG_VERSION="1.10.0"
+PKG_SHA256="8071860c1f46a75ea34801490fd1c445c9d75147a65508cd3a10366a7006cc1c"
 PKG_LICENSE="Apache"
 PKG_SITE="https://mesonbuild.com"
 PKG_URL="https://github.com/mesonbuild/meson/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
