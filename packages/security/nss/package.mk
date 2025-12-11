19de11b17335b129056a2308fa42c438c35ce77c
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-11 10:38:40 +0000

    nss: update to 3.119.1

--- packages/security/nss/package.mk
+++ packages/security/nss/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nss"
-PKG_VERSION="3.119"
-PKG_SHA256="8f12f61ef814662bf991a289bc6724d078cd79f135e09e260adbc35dfa9acaf3"
+PKG_VERSION="3.119.1"
+PKG_SHA256="1e86aacdce66c3bdd38bb011e617a3c93013ed6f802102365a589d6f686efe4b"
 PKG_LICENSE="Mozilla Public License"
 PKG_SITE="http://ftp.mozilla.org/"
-PKG_URL="https://ftp.mozilla.org/pub/security/nss/releases/NSS_${PKG_VERSION//./_}_RTM/src/nss-${PKG_VERSION}-with-nspr-$(get_pkg_version nspr).tar.gz"
+PKG_URL="https://ftp.mozilla.org/pub/security/nss/releases/NSS_${PKG_VERSION//./_}_RTM/src/nss-${PKG_VERSION//./_}-with-nspr-$(get_pkg_version nspr).tar.gz"
 PKG_DEPENDS_HOST="nspr:host zlib:host"
 PKG_DEPENDS_TARGET="make:host gcc:host nss:host nspr zlib sqlite"
 PKG_LONGDESC="The Network Security Services (NSS) package is a set of libraries designed to support cross-platform development of security-enabled client and server applications"
