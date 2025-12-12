8fbcdefb2173bcda8e2bb2874144b85ec2082865
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-12 05:30:24 +0000

    rustc-snapshot: update to 1.92.0

--- packages/rust/rustc-snapshot/package.mk
+++ packages/rust/rustc-snapshot/package.mk
@@ -10,15 +10,15 @@ PKG_TOOLCHAIN="manual"
 
 case "${MACHINE_HARDWARE_NAME}" in
   "aarch64")
-    PKG_SHA256="b9040293bc84d8b48cb859dcd0e95955c13ea29e3e637c7556bf7f0c12972217"
+    PKG_SHA256="7c8706fad4c038b5eacab0092e15db54d2b365d5f3323ca046fe987f814e7826"
     PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
   "arm")
-    PKG_SHA256="e706c9b504db51ce0b5ffb1cf39e69eb922113ef63c7e14812c7040a2f7b6db8"
+    PKG_SHA256="53406761d1774302d50007ab005ff95befc9396856389fadbc94949629bed71d"
     PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.xz"
     ;;
   "x86_64")
-    PKG_SHA256="4b4c596fc5268435c310a79c2e231a5a3567572c930ac0740ef9e147e83baf4e"
+    PKG_SHA256="78b2dd9c6b1fcd2621fa81c611cf5e2d6950690775038b585c64f364422886e0"
     PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
 esac
