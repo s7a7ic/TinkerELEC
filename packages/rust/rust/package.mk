ea7ba8068707dc50d8fce3bc5e67d05251a4cf7d
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-12 05:30:24 +0000

    rust: update to 1.92.0

--- packages/rust/rust/package.mk
+++ packages/rust/rust/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rust"
-PKG_VERSION="1.91.1"
-PKG_SHA256="38dce205d39f61571261f0444237a1ce9efecb970e760d8ec4d957af5b445723"
+PKG_VERSION="1.92.0"
+PKG_SHA256="9e0d2ca75c7e275fdc758255bf4b03afb3d65d1543602746907c933b6901c3b8"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-src.tar.gz"
@@ -33,7 +33,7 @@ configure_host() {
   esac
 
   cat >${PKG_BUILD}/config.toml  <<END
-change-id = 146435
+change-id = 147888
 
 [llvm]
 download-ci-llvm = false
