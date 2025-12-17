4e3c8c9d473540618fba0ad9a1d6450c1106b951
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-12-17 19:45:35 +0100

    flashrom: update to 1.5.1

--- packages/tools/flashrom/package.mk
+++ packages/tools/flashrom/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="flashrom"
-PKG_VERSION="1.3.0"
-PKG_SHA256="a053234453ccd012e79f3443bdcc61625cf97b7fd7cb4cdd8bfbffbe8b149623"
+PKG_VERSION="1.5.1"
+PKG_SHA256="1f934b076ed49eace203655ec249fc7861a6b8e87fe4aef732e47b6e485b6293"
 PKG_LICENSE="GPL"
-PKG_SITE="https://www.flashrom.org/Flashrom"
-PKG_URL="https://download.flashrom.org/releases/${PKG_NAME}-v${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://www.flashrom.org"
+PKG_URL="https://download.flashrom.org/releases/${PKG_NAME}-v${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libusb-compat"
 PKG_LONGDESC="flashrom is a utility for identifying, reading, writing, verifying and erasing flash chips. It is designed to flash BIOS/EFI/coreboot/firmware/optionROM images on mainboards, network/graphics/storage controller cards, and various other programmer devices."
 
