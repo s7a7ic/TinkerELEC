284260f139d2131f9cb41a06b4fd3e3d47bcc213
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-12-17 19:43:33 +0100

    mtools: update to 4.0.49

--- packages/tools/mtools/package.mk
+++ packages/tools/mtools/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtools"
-PKG_VERSION="4.0.43"
-PKG_SHA256="541e179665dc4e272b9602f2074243591a157da89cc47064da8c5829dbd2b339"
+PKG_VERSION="4.0.49"
+PKG_SHA256="6fe5193583d6e7c59da75e63d7234f76c0b07caf33b103894f46f66a871ffc9f"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/mtools/"
 PKG_URL="https://ftpmirror.gnu.org/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS_HOST="toolchain:host"
+PKG_DEPENDS_HOST="autotools:host"
 PKG_LONGDESC="mtools: A collection of utilities to access MS-DOS disks"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="-cfg-libs:host"
