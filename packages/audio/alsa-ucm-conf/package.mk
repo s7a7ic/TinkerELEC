f5ea7d93713532aa166d354985cdaef626945262
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-19 23:06:28 +0000

    alsa-ucm-conf: update to 1.2.15.1

--- packages/audio/alsa-ucm-conf/package.mk
+++ packages/audio/alsa-ucm-conf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-ucm-conf"
-PKG_VERSION="1.2.14"
-PKG_SHA256="32e9809f592d92b978aa1032e35293c33b8d0f1ec475f937022c3ee9a3069c21"
+PKG_VERSION="1.2.15.1"
+PKG_SHA256="246c5d59d217b6f7f47a11fd10f1b766d25abcb0d9c996af3079bb9c9ac5a1b0"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="https://www.alsa-project.org/"
 PKG_URL="https://www.alsa-project.org/files/pub/lib/alsa-ucm-conf-${PKG_VERSION}.tar.bz2"
