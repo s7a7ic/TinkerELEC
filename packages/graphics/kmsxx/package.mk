b2a174c3c1646de887cc0f2202e90dbde4d92403
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-22 04:59:13 +0000

    kmsxx: update to githash 9c66005

--- packages/graphics/kmsxx/package.mk
+++ packages/graphics/kmsxx/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kmsxx"
-PKG_VERSION="403c756c958c2a60adc6e8fa64aa0943b4dfda4e"
-PKG_SHA256="f8836c2a7e771c9163b7d08f22c121f134697d7d4a66d7fe9631585aff54e8db"
+PKG_VERSION="9c6600529d4e0f6c72bc2429728ab01617fb5349"
+PKG_SHA256="254794daf263565b033d742cf35c0388c040b201be4466ce1ade3bb5c924731b"
 PKG_LICENSE="MPL-2.0"
 PKG_SITE="https://github.com/tomba/kmsxx"
 PKG_URL="https://github.com/tomba/kmsxx/archive/${PKG_VERSION}.tar.gz"
