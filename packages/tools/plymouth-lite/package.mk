9cdfaf65c2da3e63a621347875691d0b32e1f7d3
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-27 02:28:12 +0000

    plymouth-lite: update to 0.6.0-5 and PKG_URL

--- packages/tools/plymouth-lite/package.mk
+++ packages/tools/plymouth-lite/package.mk
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="plymouth-lite"
-PKG_VERSION="0.6.0"
-PKG_SHA256="fa7b581bdd38c5751668243ff9d2ebaee7c45753358cbb310fb50cfcd3a8081b"
+PKG_VERSION="0.6.0-5"
+PKG_SHA256="476816c34bb75715b3709a7ed0d29d93e63624858f1803e5746dc350b6081607"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.meego.com"
-PKG_URL="${DISTRO_SRC}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://github.com/sailfishos/plymouth-lite"
+PKG_URL="https://github.com/sailfishos/plymouth-lite/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_INIT="toolchain gcc:init libpng"
 PKG_LONGDESC="Boot splash screen based on Fedora's Plymouth code"
 
