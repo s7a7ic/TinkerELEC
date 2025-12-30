691add2316c41a74328a5559b4690b66461325c8
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-30 22:26:43 +0000

    bcm2835-utils: update to githash 230d67a
    Log:
    - https://github.com/raspberrypi/utils/compare/c3d0bfe3a8fc3404bebf2fe149b1bf595ee0f30b...230d67ad28e74b17a42064453b2163991cb51a5e

--- packages/tools/bcm2835-utils/package.mk
+++ packages/tools/bcm2835-utils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcm2835-utils"
-PKG_VERSION="c3d0bfe3a8fc3404bebf2fe149b1bf595ee0f30b"
-PKG_SHA256="10a36f19bd9d25aa267fc68c35cc56956312c19fdcc05f57459bb0a5f9c0561a"
+PKG_VERSION="230d67ad28e74b17a42064453b2163991cb51a5e"
+PKG_SHA256="4901d4b8729afb6dd7de39333311ac575d56f0a7ed92c4efd4af4b664bad4eb3"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="BSD-3-Clause"
 PKG_SITE="https://github.com/raspberrypi/utils"
