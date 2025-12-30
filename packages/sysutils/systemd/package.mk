7e7f56c9dafa149aee79cd6e1833d0ce8ce3b440
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-12-30 13:36:34 +0100

    systemd: disable userdbd nss-systemd
    This reverts commit 0f8d8410c50daff1f76614b6ab6f83908f4dd571
    I don't see any reason why this "service" should be running.
    I did not find any existing requirement or any practical usecase for this "service" on a "Just enough OS for KODI"...

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -63,7 +63,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
                        -Dlocaled=false \
                        -Dmachined=false \
                        -Dportabled=false \
-                       -Duserdb=true \
+                       -Duserdb=false \
                        -Dnologin-path=/usr/sbin/nologin \
                        -Dhomed=disabled \
                        -Dnetworkd=false \
@@ -87,7 +87,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
                        -Dnss-myhostname=false \
                        -Dnss-mymachines=disabled \
                        -Dnss-resolve=disabled \
-                       -Dnss-systemd=true \
+                       -Dnss-systemd=false \
                        -Dman=disabled \
                        -Dhtml=disabled \
                        -Dlink-udev-shared=true \
@@ -334,7 +334,7 @@ post_install() {
   enable_service network-base.service
   enable_service systemd-timesyncd.service
   enable_service systemd-timesyncd-setup.service
-  enable_service systemd-userdbd.socket
+  #enable_service systemd-userdbd.socket
   if [ "${LOCAL_LOGIN}" = "yes" ]; then
     enable_service getty@tty0.service
   fi
