0e39f78ad1032732594d61f57c3de7468560320c
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-01-07 11:57:09 +0100

    Pillow: add missing pybind11 depency

--- packages/python/graphics/Pillow/package.mk
+++ packages/python/graphics/Pillow/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="8160e8b97c9e12337073b91d427d88df9490ba500f21d32015cfbb6f98846670"
 PKG_LICENSE="BSD"
 PKG_SITE="https://python-pillow.org/"
 PKG_URL="https://github.com/python-pillow/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3 zlib freetype libjpeg-turbo tiff"
+PKG_DEPENDS_TARGET="toolchain pybind11:host Python3 zlib freetype libjpeg-turbo tiff"
 PKG_LONGDESC="The Python Imaging Library adds image processing capabilities to your Python interpreter."
 PKG_TOOLCHAIN="python"
 
