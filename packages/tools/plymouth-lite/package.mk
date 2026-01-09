c054a35cb8591c1722d515795900960f1644462c
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-09 08:33:30 +0000

    plymouth-lite: update to githash d41ce34

--- packages/tools/plymouth-lite/package.mk
+++ packages/tools/plymouth-lite/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="plymouth-lite"
-PKG_VERSION="9e2ff28ea32e8ab0410435d25e198136e6ad82dc"
-PKG_SHA256="1efbcf4758795791e45bf3512c44bf5898acd6072369189ffbdbb07f470156e8"
+PKG_VERSION="d41ce34de554a1cae3250e6a945822ed12717eb5"
+PKG_SHA256="ee914d57ac8e8c9b4ab238dc3dd1d7e461b62896408e55b8ac737b5edaa8eaca"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/sailfishos/plymouth-lite"
 PKG_URL="https://github.com/sailfishos/plymouth-lite/archive/${PKG_VERSION}.tar.gz"
