e1abf953a1dbb913d43ef0d009768d1252fc6517
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-10 22:37:23 +0000

    kernel-firmware: update to 20260110

--- packages/linux-firmware/kernel-firmware/package.mk
+++ packages/linux-firmware/kernel-firmware/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kernel-firmware"
-PKG_VERSION="20251125"
-PKG_SHA256="eb807a01c52882ac97ef5b678d4a246b209e6165ac1287d62a5f93a09ee93cd2"
+PKG_VERSION="20260110"
+PKG_SHA256="48e05166db539f4ee8d29ac9d236a94442c5b1b1a160a966f6fe6b42bd714331"
 PKG_LICENSE="other"
 PKG_SITE="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/"
 PKG_URL="https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${PKG_VERSION}.tar.xz"
