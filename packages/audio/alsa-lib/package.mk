b572b19476b1fa8395c3fb5b6a4049ec43cfd210
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-13 09:48:40 +0000

    alsa-lib: update to 1.2.15.3
    - https://www.alsa-project.org/wiki/Changes_v1.2.15.2_v1.2.15.3

--- packages/audio/alsa-lib/package.mk
+++ packages/audio/alsa-lib/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-lib"
-PKG_VERSION="1.2.15.2"
-PKG_SHA256="637eefd4966ce738da44464494df2b2894e19778fac2f9e7c47277e2af9297f4"
+PKG_VERSION="1.2.15.3"
+PKG_SHA256="7b079d614d582cade7ab8db2364e65271d0877a37df8757ac4ac0c8970be861e"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.alsa-project.org/"
 PKG_URL="https://www.alsa-project.org/files/pub/lib/alsa-lib-${PKG_VERSION}.tar.bz2"
