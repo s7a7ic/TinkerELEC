e939b7266fee8dea76f1b141c5da58c6b3194e7c
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-21 21:16:46 +0000

    pycparser: update to 3.0

--- packages/python/devel/pycparser/package.mk
+++ packages/python/devel/pycparser/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pycparser"
-PKG_VERSION="2.23"
-PKG_SHA256="78816d4f24add8f10a06d6f05b4d424ad9e96cfebf68a4ddc99c65c0720d00c2"
+PKG_VERSION="3.0"
+PKG_SHA256="600f49d217304a5902ac3c37e1281c9fe94e4d0489de643a9504c5cdfdfc6b29"
 PKG_LICENSE="BSD-3-Clause"
 PKG_SITE="https://pypi.org/project/pycparser/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
