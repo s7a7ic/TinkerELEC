e7f016a56eb7d92a20a51f76817338b3dcd1bb05
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-22 10:13:21 +0000

    iwlwifi-firmware: update to githash 83a591b

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="e740611a3f9a7a6673a68ad2a3b9963e7dfdcdbb"
-PKG_SHA256="bbd74b470553836d47700950cef06d4e21bc01c24bcd71227211cff3c4f34b3f"
+PKG_VERSION="83a591b8b69e31e1d880355cf5ee7fe91b74b0ad"
+PKG_SHA256="ecaed1c3ad54c4fd066c02c5ccd3d99f8f839f4917c6204d2040fd1df0c9b52c"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
