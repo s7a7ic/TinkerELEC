7e41888417ccd25e2a6a0d26ff7a6653e48d414c
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-23 03:08:22 +0000

    groovy: update to 4.0.30

--- packages/devel/groovy/package.mk
+++ packages/devel/groovy/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="groovy"
-PKG_VERSION="4.0.29"
-PKG_SHA256="4a42d976370c6ab373a35ec602440a8a780a7715d55e4117b3028864a247878a"
+PKG_VERSION="4.0.30"
+PKG_SHA256="00ff6f78e8a042def489e6c018f2353895da99936b73c175ea645d536a9acb66"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://groovy.apache.org"
 PKG_URL="https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-${PKG_VERSION}.zip"
