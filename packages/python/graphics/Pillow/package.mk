5e3fc37bf4acb4a27e5a549c493fb8ba74252437
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-01-27 00:18:31 +0100

    Pillow: downgrade to 11.3.0

--- packages/python/graphics/Pillow/package.mk
+++ packages/python/graphics/Pillow/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Pillow"
-PKG_VERSION="12.1.0"
-PKG_SHA256="8160e8b97c9e12337073b91d427d88df9490ba500f21d32015cfbb6f98846670"
+PKG_VERSION="11.3.0"
+PKG_SHA256="fa4aca745b1e1c733589ebf0ef19491b145dd4225c4aa06958963b4e7f0734cf"
 PKG_LICENSE="BSD"
 PKG_SITE="https://python-pillow.org/"
 PKG_URL="https://github.com/python-pillow/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain pybind11:host Python3 zlib freetype libjpeg-turbo tiff"
+PKG_DEPENDS_TARGET="toolchain Python3 zlib freetype libjpeg-turbo tiff"
 PKG_LONGDESC="The Python Imaging Library adds image processing capabilities to your Python interpreter."
 PKG_TOOLCHAIN="python"
 
