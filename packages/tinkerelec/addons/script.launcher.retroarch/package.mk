4a43b94b98db31ea1b9ccdfcebbaf1d908000f8d
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-01-27 23:07:34 +0100

    script.launcher.retroarch: update package.mk

--- packages/tinkerelec/addons/script.launcher.retroarch/package.mk
+++ packages/tinkerelec/addons/script.launcher.retroarch/package.mk
@@ -5,7 +5,7 @@ PKG_VERSION="1.22.2"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
-PKG_DEPENDS_TARGET="retroarch"
+PKG_DEPENDS_TARGET="retroarch retroarch_assets retroarch_joypad_autoconfig"
 PKG_SECTION=""
 PKG_SHORTDESC="Launch RetroArch (${PKG_VERSION})"
 PKG_LONGDESC="RetroArch is a frontend for emulators, game engines and media players. The add-on provides binary, cores and basic settings to launch RetroArch from Kodi UI, plus additional features to improve user experience."
@@ -13,20 +13,17 @@ PKG_TOOLCHAIN="manual"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="RetroArch"
-PKG_ADDON_TYPE="xbmc.python.script"
+PKG_ADDON_TYPE="xbmc.simple"
+PKG_ADDON_EXTENSION="xbmc.python.pluginsource"
 PKG_ADDON_PROVIDES="executable game"
 PKG_MAINTAINER="s7a7ic"
 
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib}
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib,resources}
 
-#  patchelf --add-rpath '${ORIGIN}/../lib.private' ${ADDON_BUILD}/${PKG_ADDON_ID}/bin/snapclient
+  cp $(get_install_dir retroarch)/usr/bin/retroarch ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  cp -r $(get_install_dir retroarch_assets)/usr/share/retroarch/assets ${ADDON_BUILD}/${PKG_ADDON_ID}/resources/assets
 
-#  if [ "${ALSA_SUPPORT}" = yes ]; then
-#    cp $(get_install_dir alsa-plugins)/usr/lib/alsa/*.so \
-#       ${ADDON_BUILD}/${PKG_ADDON_ID}/lib.private
-#  fi
-
-  cp $(get_install_dir retroarch)/usr/bin/retroarch \
-     ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  mkdir ${ADDON_BUILD}/${PKG_ADDON_ID}/resources/joypads
+  cp -r $(get_install_dir retroarch_joypad_autoconfig)/etc/retroarch-joypad-autoconfig/* ${ADDON_BUILD}/${PKG_ADDON_ID}/resources/joypads
 }
