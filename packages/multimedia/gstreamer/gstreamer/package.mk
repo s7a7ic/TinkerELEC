606a5d5e4f4898ba830a1add09c632c2fd8977e0
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-28 05:10:07 +0000

    gstreamer: update to 1.28.0

--- packages/multimedia/gstreamer/gstreamer/package.mk
+++ packages/multimedia/gstreamer/gstreamer/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gstreamer"
-PKG_VERSION="1.27.2"
-PKG_SHA256="ce129c7ea92dce3063b029171d1324d203927132fc3e0cf92b784ddd055c241d"
+PKG_VERSION="1.28.0"
+PKG_SHA256="6c8676bc39a2b41084fd4b21d2c37985c69ac979c03ce59575db945a3a623afd"
 PKG_LICENSE="GPL-2.1-or-later"
 PKG_SITE="https://gstreamer.freedesktop.org"
 PKG_URL="https://gstreamer.freedesktop.org/src/gstreamer/${PKG_NAME}-${PKG_VERSION}.tar.xz"
