bc06caf9c5086afbfe4615b22534c05be407930a
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2026-01-29 10:18:56 +0100

    eglexternalplatform: header dependencies for nvidia/egl-gbm
    header files required to compile libnvidia-egl-gbm, that is now provided
    separatly from the nvidia driver

---
+++ packages/graphics/eglexternalplatform/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="eglexternalplatform"
+PKG_VERSION="1.2"
+PKG_SHA256="aba414e6f441b1253684b4bb838b3fecde2a851374a71f32a7ba2ca80daf1709"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/NVIDIA/eglexternalplatform"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="NVIDIA EGL External Platform interface headers"
+PKG_TOOLCHAIN="meson"
