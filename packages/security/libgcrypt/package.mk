8ab313495730c9ed2101b2c0d6e8e346fab2e777
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-29 21:35:23 +0000

    libgcrypt: update to 1.12.0

--- packages/security/libgcrypt/package.mk
+++ packages/security/libgcrypt/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libgcrypt"
-PKG_VERSION="1.11.2"
-PKG_SHA256="6ba59dd192270e8c1d22ddb41a07d95dcdbc1f0fb02d03c4b54b235814330aac"
+PKG_VERSION="1.12.0"
+PKG_SHA256="0311454e678189bad62a7e9402a9dd793025efff6e7449898616e2fc75e0f4f5"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.gnupg.org/"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/libgcrypt/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
