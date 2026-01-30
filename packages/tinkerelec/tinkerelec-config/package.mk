49baac68426632b70f82d9a8e030cd84379d75b4
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-01-30 17:34:44 +0100

    tinkerelec-config: rename package & add configs
    rename package from "tinkerelec-init-config" to "tinkerelec-config"
    add openssh config for terminating suspended sessions
    moved libinput rules for nespi case buttons from kodi package

---
+++ packages/tinkerelec/tinkerelec-config/package.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+PKG_NAME="tinkerelec-config"
+PKG_VERSION="1.0"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/s7a7ic/TinkerELEC-Project"
+PKG_URL=""
+PKG_DEPENDS_TARGET="kodi systemd"
+PKG_DEPENDS_UNPACK="kodi"
+PKG_LONGDESC="This package configures and modifies TinkerELEC for my specific usecase."
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/share/kodi/addons/peripheral.joystick/resources/buttonmaps/xml/linux/
+  cp -a ${PKG_DIR}/files/*.xml ${INSTALL}/usr/share/kodi/addons/peripheral.joystick/resources/buttonmaps/xml/linux/
+}
+
+post_makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/config
+  cp -PR ${PKG_DIR}/config/* ${INSTALL}/usr/config
+
+  mkdir -p ${INSTALL}/etc/ssh/ssh_config.d
+  cp ${PKG_DIR}/ssh_config.d/*.conf ${INSTALL}/etc/ssh/ssh_config.d
+
+  # watchdog configuration
+  sed -e "s,^.*RuntimeWatchdogSec=.*$,RuntimeWatchdogSec=20,g" -i $(get_install_dir systemd)/etc/systemd/system.conf
+  sed -e "s,^.*RebootWatchdogSec=.*$,RebootWatchdogSec=5min,g" -i $(get_install_dir systemd)/etc/systemd/system.conf
+  #sed -e "s,^.*ShutdownWatchdogSec=.*$,ShutdownWatchdogSec=5min,g" -i $(get_install_dir systemd)/etc/systemd/system.conf
+
+  # nespi case reboot and power key handling
+  sed -e "s,^.*HandleRebootKey=.*$,HandleRebootKey=suspend,g" -i $(get_install_dir systemd)/etc/systemd/logind.conf
+  sed -e "s,^.*HandleRebootKeyLongPress=.*$,HandleRebootKeyLongPress=reboot,g" -i $(get_install_dir systemd)/etc/systemd/logind.conf
+}
