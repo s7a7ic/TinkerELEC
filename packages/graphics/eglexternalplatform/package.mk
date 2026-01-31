41983d7697f82946ab516a0213b063003f0ac03a
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-31 02:07:07 +0000

    eglexternalplatform: update to 1.2.1

--- packages/graphics/eglexternalplatform/package.mk
+++ packages/graphics/eglexternalplatform/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="eglexternalplatform"
-PKG_VERSION="1.2"
-PKG_SHA256="aba414e6f441b1253684b4bb838b3fecde2a851374a71f32a7ba2ca80daf1709"
+PKG_VERSION="1.2.1"
+PKG_SHA256="5089ceb054ca50c85837f015756a3d0f2f75cf2a98c9e5fbcbcfb8206137f76e"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/NVIDIA/eglexternalplatform"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
