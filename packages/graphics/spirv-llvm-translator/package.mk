f348026a99270747c2e7afed964650c28535953c
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-02 01:59:30 +0000

    spirv-llvm-translator: update to 21.1.4

--- packages/graphics/spirv-llvm-translator/package.mk
+++ packages/graphics/spirv-llvm-translator/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="spirv-llvm-translator"
-PKG_VERSION="21.1.3"
-PKG_SHA256="43080fd5122c71cd93a3d174d59b9fc95ff8aeb1847d50f394088112f6b2a217"
+PKG_VERSION="21.1.4"
+PKG_SHA256="e900c907b62f82cf541a611b33d6685f375fe3bda6f041087fcdba350ffb437d"
 PKG_LICENSE="LLVM"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/v${PKG_VERSION}.tar.gz"
