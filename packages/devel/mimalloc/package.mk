604c63de4a7fa3f3db19c4f8eb0908aa8e900b42
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-04 08:28:26 +0000

    mimalloc: update to 3.2.8

--- packages/devel/mimalloc/package.mk
+++ packages/devel/mimalloc/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mimalloc"
-PKG_VERSION="3.2.7"
-PKG_SHA256="33621bea902711639b8f83128e64685ffc8224a65443625530747603a2d8726d"
+PKG_VERSION="3.2.8"
+PKG_SHA256="68163666575518c213a6593850099adce3863b340ca2751103dbd1f253664e05"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/microsoft/mimalloc"
 PKG_URL="https://github.com/microsoft/mimalloc/archive/refs/tags/v${PKG_VERSION}.tar.gz"
