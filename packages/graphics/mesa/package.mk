6a12b980ef69baf2eb7a0e7eb6929278b0292acc
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-07 01:02:02 +0000

    mesa: update to 25.3.5
    Release notes:
    - https://gitlab.freedesktop.org/mesa/mesa/-/blob/main/docs/relnotes/25.3.5.rst

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="25.3.4"
-PKG_SHA256="3a0fc6ec070b45ae25dc2ccb5e52fae1d89141f7c39c4a91fe4eaa80dfff9deb"
+PKG_VERSION="25.3.5"
+PKG_SHA256="be472413475082df945e0f9be34f5af008baa03eb357e067ce5a611a2d44c44b"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
