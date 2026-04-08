499ba52a1b651d6105a9b8511c1e19638d0e94f4
Rudi Heitbaum <rudi@heitbaum.com>
2026-04-08 01:50:46 +0000

    openssl: update to 3.5.6
    Release notes:
    - https://www.openssl.org
    
    Release notes:
    - https://www.openssl.org/news/vulnerabilities.html

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssl"
-PKG_VERSION="3.5.5"
-PKG_SHA256="b28c91532a8b65a1f983b4c28b7488174e4a01008e29ce8e69bd789f28bc2a89"
+PKG_VERSION="3.5.6"
+PKG_SHA256="deae7c80cba99c4b4f940ecadb3c3338b13cb77418409238e57d7f31f2a3b736"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://openssl-library.org"
 PKG_URL="https://github.com/openssl/openssl/releases/download/${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
