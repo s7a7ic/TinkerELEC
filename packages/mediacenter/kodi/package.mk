148ea9cab03e013f6f7147b367b33bce36b0cb1c
Christian Hewitt <christianshewitt@gmail.com>
2026-05-20 11:26:19 +0000

    kodi: bump to 0f161dd3
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="839f5cf8ef19ea5840a57329bff20eac44857d13"
-PKG_SHA256="80695a343897a5383fb35136125786cadcb91cc7e3b16eb7ac54b22b496de789"
+PKG_VERSION="0f161dd3cb5c6c6fbc1a1c53905aa3eed4e1b923"
+PKG_SHA256="2a194ea7a2f20494f781e9d0c316962a3d0b5865ccc15c309632e81b32fe0370"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
