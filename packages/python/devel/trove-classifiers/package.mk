5630a3456dc5e1231585c8a4f02b452d4a5f15ea
Rudi Heitbaum <rudi@heitbaum.com>
2026-05-21 11:30:34 +0000

    trove-classifiers: update to 2026.5.20.19

--- packages/python/devel/trove-classifiers/package.mk
+++ packages/python/devel/trove-classifiers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="trove-classifiers"
-PKG_VERSION="2026.5.7.17"
-PKG_SHA256="c5d469f4623f3a361f960532feafabb8d30a63d987c9815f380450ad72c56f33"
+PKG_VERSION="2026.5.20.19"
+PKG_SHA256="c04d2431fb42e316c302cce63064ede9801bac498b2bbeb81ee7d2711ba81113"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/pypa/trove-classifiers"
 PKG_URL="https://github.com/pypa/trove-classifiers/archive/refs/tags/${PKG_VERSION}.tar.gz"
