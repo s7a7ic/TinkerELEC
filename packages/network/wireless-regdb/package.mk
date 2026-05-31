2136fa762fcd622b5b14ae8804a07a637ab5a862
Rudi Heitbaum <rudi@heitbaum.com>
2026-05-31 01:41:53 +0000

    wireless-regdb: update to 2026.05.30
    Release notes:
    - https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git

--- packages/network/wireless-regdb/package.mk
+++ packages/network/wireless-regdb/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wireless-regdb"
-PKG_VERSION="2026.02.04"
-PKG_SHA256="0ff48a5cd9e9cfe8e815a24e023734919e9a3b7ad2f039243ad121cf5aabf6c6"
+PKG_VERSION="2026.05.30"
+PKG_SHA256="8a27bfc081bafed8c24dd70fab0d96f098e5a0bfcd08d3da672595f225ab8993"
 PKG_LICENSE="GPL"
 PKG_SITE="https://wireless.wiki.kernel.org/en/developers/regulatory/wireless-regdb"
 PKG_URL="https://www.kernel.org/pub/software/network/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
