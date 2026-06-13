7694de03ce8ff43c83aff2d6720dddd94d1de88c
Rudi Heitbaum <rudi@heitbaum.com>
2026-06-13 20:13:27 +1000

    cxxtools: update to 264edce and switch to cmake
    The tntnet.org website is offline; switch PKG_SITE/PKG_URL to GitHub
    (maekitalo/cxxtools) and update PKG_VERSION to HEAD (264edce).
    
    Drop patches now merged in HEAD: trivial-class ctor, ctime-in-timer,
    openssl-lld and C++20 allocator fix. Drop unnecessary patches 0001-0003,
    0039.
    
    Switch PKG_TOOLCHAIN to cmake; replace PKG_CONFIGURE_OPTS_* with
    PKG_CMAKE_OPTS_* and drop the post_makeinstall_host cxxtools-config
    hook which cmake no longer requires.
    
    Add patch 0001 to skip demos/tests and add library/header/pkg-config
    install() rules.
    
    All patches upstreamed at https://github.com/maekitalo/cxxtools/pulls
    including hygiene and clean up patches.

--- packages/addons/addon-depends/cxxtools/package.mk
+++ packages/addons/addon-depends/cxxtools/package.mk
@@ -3,27 +3,19 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cxxtools"
-PKG_VERSION="3.0"
-PKG_SHA256="07b18037fb0983f6292f5c8d53e2369e9e7a9711df2c9ad50838aacbc8c62f7c"
+PKG_VERSION="264edcee6ef224b8db17446f0b761e75ae56fff8"
+PKG_SHA256="75ca10ad4d1a353a691de15516d3ef681f4aa6d31ccd54529cd0e8d328f896f8"
 PKG_LICENSE="LGPL-2.1-or-later"
-PKG_SITE="http://www.tntnet.org/cxxtools.html"
-PKG_URL="http://www.tntnet.org/download/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/maekitalo/cxxtools"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="toolchain:host openssl:host"
 PKG_DEPENDS_TARGET="toolchain cxxtools:host openssl"
-PKG_LONGDESC="Cxxtools is a collection of general-purpose C++ classes."
+PKG_LONGDESC="Cxxtools is a collection of general-purpose C++ components."
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-demos --disable-unittest"
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --disable-demos --disable-unittest"
-
-post_makeinstall_host() {
-  rm -rf ${TOOLCHAIN}/bin/cxxtools-config
-}
+PKG_CMAKE_OPTS_HOST="-DBUILD_SHARED_LIBS=ON -DBUILD_DEMOS=OFF -DBUILD_TESTS=OFF"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF -DBUILD_DEMOS=OFF -DBUILD_TESTS=OFF"
 
 post_makeinstall_target() {
-  cp ${PKG_NAME}-config ${TOOLCHAIN}/bin
-  sed -e "s:\(['= ]\)/usr:\\1${PKG_ORIG_SYSROOT_PREFIX}/usr:g" -i ${TOOLCHAIN}/bin/${PKG_NAME}-config
-  chmod +x ${TOOLCHAIN}/bin/${PKG_NAME}-config
-
   rm -rf ${INSTALL}/usr/bin
 }
