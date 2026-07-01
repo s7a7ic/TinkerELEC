8ad8b430e36fc43854dfd54358704c70d25bd1f4
Rudi Heitbaum <rudi@heitbaum.com>
2026-07-01 08:26:35 +0000

    inputstream.adaptive: update 21.5.19-Omega to 21.5.22-Omega

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="21.5.19-Omega"
-PKG_SHA256="20922de1891c9980f9807a10edbe2d1ae9224c298c438a3458d7b7105d5b7c21"
+PKG_VERSION="21.5.22-Omega"
+PKG_SHA256="8c9132597fc5d047657f10873c09f9f6837b73f140b7a59b8d72929d37fc79f2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
