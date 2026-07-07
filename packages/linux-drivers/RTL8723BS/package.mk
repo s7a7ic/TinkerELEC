b966002da74bca7f4f7a1229d5a3aeaeb15be09e
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-07-07 21:06:20 +0200

    RTL8723BS: update package

--- packages/linux-drivers/RTL8723BS/package.mk
+++ packages/linux-drivers/RTL8723BS/package.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="RTL8723BS"
-PKG_VERSION="5a3f3ee2edd6cd67276b01553ef3f221c5930eca"
-PKG_SHA256="1ba77ebff7e34eb0bf8666a533ee33929d8b4da564c2aaa901a0d5a565f6dbee"
+PKG_VERSION="71ef48440677ad072aa1e7728a3410edcbdf9b97"
+PKG_SHA256="da46ebe16fa11eb2f5705a3ea2f717b7ba3d7894e3e62cf4f0555d5cb37b122c"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/s7a7ic/rockchip_wlan"
-PKG_URL="https://github.com/s7a7ic/rockchip_wlan/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/s7a7ic/rtl8723bs_linux_driver"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Realtek RTL8723BS Linux driver"
 PKG_IS_KERNEL_PKG="yes"
 PKG_DEPENDS_TARGET="autotools:host gcc:host linux"
@@ -14,15 +14,10 @@ PKG_TOOLCHAIN="manual"
 PKG_BUILD_FLAGS="+lto"
 
 make_target() {
-  kernel_make V=1 -C $(kernel_path) M="${PKG_BUILD}/rtl8723bs"
+  kernel_make V=1 -C $(kernel_path) M="${PKG_BUILD}"
 }
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtl8723bs
-  cp ${PKG_BUILD}/rtl8723bs/*.ko ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtl8723bs
+  cp ${PKG_BUILD}/*.ko ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtl8723bs
 }
-
-#post_makeinstall_target() {
-#  mkdir -p ${INSTALL}/usr/config
-#  cp -PR ${PKG_DIR}/config/modprobe.d ${INSTALL}/usr/config
-#}
