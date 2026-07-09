eabcb2b8f32ebc37629e37f2dc7ce4fd4456d1d6
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-07-09 23:10:23 +0200

    mupen64plus-nx: use patchelf to clear execstack

--- packages/emulation/libretro-mupen64plus-nx/package.mk
+++ packages/emulation/libretro-mupen64plus-nx/package.mk
@@ -61,5 +61,6 @@ fi
 makeinstall_target() {
   mkdir -p ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}
   cp ${PKG_LIBPATH} ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME}
+  patchelf --clear-execstack ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME}
   echo "set(${PKG_LIBVAR} ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME})" > ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}/${PKG_NAME}-config.cmake
 }
