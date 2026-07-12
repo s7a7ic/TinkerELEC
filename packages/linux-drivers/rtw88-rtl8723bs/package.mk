c0294f046da14dd7deca01a8e8af4f54f04fffe3
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-07-12 23:19:49 +0200

    rtw88-rtl8723bs: copy only the required files into final image

--- packages/linux-drivers/rtw88-rtl8723bs/package.mk
+++ packages/linux-drivers/rtw88-rtl8723bs/package.mk
@@ -18,9 +18,9 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtw88
-  cp ${PKG_BUILD}/rtw_8723*.ko ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtw88
-  cp ${PKG_BUILD}/rtw_core.ko ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtw88
-  cp ${PKG_BUILD}/rtw_sdio.ko ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtw88
+  cp ${PKG_BUILD}/rtw_8723x.ko ${PKG_BUILD}/rtw_8723b.ko ${PKG_BUILD}/rtw_8723bs.ko \
+    ${PKG_BUILD}/rtw_core.ko ${PKG_BUILD}/rtw_sdio.ko \
+    ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtw88
   mkdir -p ${INSTALL}/$(get_kernel_overlay_dir)/lib/firmware/rtw88/
     cp -av ${PKG_BUILD}/firmware/rtw8723b_*.bin ${INSTALL}/$(get_kernel_overlay_dir)/lib/firmware/rtw88/
 }
