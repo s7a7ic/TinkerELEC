85ba12aa71344954fc26482dcf1814b3cffd125b
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-07-13 11:31:38 +0200

    rtw88-rtl8723bs: update to latest
    don't copy firmware file, as it's already included in image
    set package license variable to GPLv2
    change package description

--- packages/linux-drivers/rtw88-rtl8723bs/package.mk
+++ packages/linux-drivers/rtw88-rtl8723bs/package.mk
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="rtw88-rtl8723bs"
-PKG_VERSION="c6ea9993949fbe98b03d27567160cd85a0c4265e"
-PKG_SHA256="e19c9d2eb3f9637315b8d8340f737e9cce73dbb9f8f01ff0017dbc33fc3b5251"
-PKG_LICENSE="GPL"
+PKG_VERSION="d74a40c3e18f35f91691a509ec21a78a086ddba3"
+PKG_SHA256="a349a235addc6d76e817e9f43dfd63138ca5eb09e6c6912f2d952a1551b4da5a"
+PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/MocLG/rtw88-rtl8723bs"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
-PKG_LONGDESC="Realtek RTL8723BS Linux driver"
+PKG_LONGDESC="rtw88 driver for the Realtek RTL8723BS"
 PKG_IS_KERNEL_PKG="yes"
 PKG_DEPENDS_TARGET="autotools:host gcc:host linux"
 PKG_NEED_UNPACK="${LINUX_DEPENDS}"
@@ -21,6 +21,4 @@ makeinstall_target() {
   cp ${PKG_BUILD}/rtw_8723x.ko ${PKG_BUILD}/rtw_8723b.ko ${PKG_BUILD}/rtw_8723bs.ko \
     ${PKG_BUILD}/rtw_core.ko ${PKG_BUILD}/rtw_sdio.ko \
     ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/realtek/rtw88
-  mkdir -p ${INSTALL}/$(get_kernel_overlay_dir)/lib/firmware/rtw88/
-    cp -av ${PKG_BUILD}/firmware/rtw8723b_*.bin ${INSTALL}/$(get_kernel_overlay_dir)/lib/firmware/rtw88/
 }
