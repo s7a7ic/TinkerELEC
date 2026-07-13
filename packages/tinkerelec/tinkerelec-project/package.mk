9962b6851e4c4544f4305f83e3a4aa80fbb7595b
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-07-13 23:41:07 +0200

    tinkerelec-project: update package

--- packages/tinkerelec/tinkerelec-project/package.mk
+++ packages/tinkerelec/tinkerelec-project/package.mk
@@ -1,19 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="tinkerelec-project"
-PKG_VERSION="9ae6bc5b88c0f06133c5acd0ed6275c13fc01f41"
+PKG_VERSION="6243688916239f5f8c88ec02ac230b8697e75b78"
+PKG_SHA256="f472095b64e9668a9b51e5b711a678256003d079577a2bce7a5b9073a35393f6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/s7a7ic/TinkerELEC-Project"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="This package pulls the TinkerELEC configuration for the addon."
 PKG_TOOLCHAIN="manual"
-
-#makeinstall_target() {
-#  mkdir -p ${INSTALL}/usr/share/kodi/addons/peripheral.joystick/resources/buttonmaps/xml/linux/
-#  cp -a ${PKG_DIR}/files/*.xml ${INSTALL}/usr/share/kodi/addons/peripheral.joystick/resources/buttonmaps/xml/linux/
-#}
-
-#post_makeinstall_target() {
-#  mkdir -p ${INSTALL}/usr/config
-#  cp -PR ${PKG_DIR}/config/* ${INSTALL}/usr/config
-#}
